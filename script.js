3bb63a9ce5297404c87d0ba7fcbcab042b0ad313
“[Amri Iqra Samudra Al-Fatihah <amriiqosamudraalfatihah@gmail.com>
2022-09-29 18:21:25 +0700

    style: [AI] add sponsor and footer

--- script.js
+++ script.js
@@ -24,3 +24,26 @@ function callbackFunc() {
 
  window .addEventListener("load", callbackFunc);
  window.addEventListener("scroll", callbackFunc);
+
+ $(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover:false,
+        responsive: [{
+            breakpoint: 768,
+            setting: {
+                slidesToShow:4
+            }
+        }, {
+            breakpoint: 520,
+            setting: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
