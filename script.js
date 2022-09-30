2632532a28b3f6978719cc933f66616c3ca04d9e
“[Amri Iqra Samudra Al-Fatihah <amriiqosamudraalfatihah@gmail.com>
2022-09-30 08:16:59 +0700

    style [AI] add media query

--- script.js
+++ script.js
@@ -34,16 +34,28 @@ function callbackFunc() {
         arrows: false,
         dots: false,
         pauseOnHover:false,
-        responsive: [{
-            breakpoint: 768,
-            setting: {
-                slidesToShow:4
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
             }
-        }, {
-            breakpoint: 520,
-            setting: {
-                slidesToShow: 3
-            }
-        }]
+    ]
     });
 });
