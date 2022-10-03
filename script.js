d95d7c8ef0134e4c98a5915bd1957ba0d078f3dc
“[Amri Iqra Samudra Al-Fatihah <amriiqosamudraalfatihah@gmail.com>
2022-10-03 18:41:39 +0700

    style: [AI] fix bug and change background

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-var testimonialItems = document.querySelectorAll(".item label");
-var timer;
-function cycleTestimonials(index) {
-   timer = setTimeout(function() {
-    var evt;
-    if (document.createEvent){
-      
-      evt = document.createEvent('MouseEvent');
-      evt.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    } else {
-      
-      evt = new MouseEvent("click", {
-            view: window,
-            bubbles: true,
-            cancelable: true,
-            clientX: 20
-          });
-    }
-    var ele = "." + testimonialItems[index].className;
-    var ele2 = document.querySelector(ele)
-    ele2.dispatchEvent(evt);
-    index++;
-    if (index >= testimonialItems.length) {
-      index = 0;
-    }
-    cycleTestimonials(index);
-    document.querySelector(".testimonials").addEventListener("click", function() {
-      clearTimeout(timer);
-    });
-  }, 5000);
-}
-
-//menjalankan function
-cycleTestimonials(0);
-
 var items = document.querySelectorAll(".timeline li");
 
 function isElementInViewport(el){
@@ -140,3 +105,41 @@ function scrollFunction() {
   };
 
 // END NAVBAR MELAYANG -----
+
+// SLIDER ------
+var testimonialItems = document.querySelectorAll(".item label");
+var timer;
+function cycleTestimonials(index) {
+   timer = setTimeout(function() {
+    var evt;
+    if (document.createEvent){
+      
+      evt = document.createEvent('MouseEvent');
+      evt.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    } else {
+      
+      evt = new MouseEvent("click", {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+            clientX: 20
+          });
+    }
+    var ele = "." + testimonialItems[index].className;
+    var ele2 = document.querySelector(ele)
+    ele2.dispatchEvent(evt);
+    index++;
+    if (index >= testimonialItems.length) {
+      index = 0;
+    }
+    cycleTestimonials(index);
+    document.querySelector(".testimonials").addEventListener("click", function() {
+      clearTimeout(timer);
+    });
+  }, 5000);
+}
+
+//menjalankan function
+cycleTestimonials(0);
+
+// END SLIDER ------
